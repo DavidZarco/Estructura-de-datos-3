0557583ba11c1cd22a48d7838b9cda4562301d5b
David Zarcó <davidzarcofca@gmail.com>
2022-04-20 20:36:59 -0300

    PartidosAdmin

--- Ejercicio1/Partido.java
+++ Ejercicio1/Partido.java
@@ -1,5 +1,13 @@
 package Ejercicio1;
 
 public class Partido {
-    
+Fecha LocalDate;
+Equipo equipoLocal;
+Equipo equipoVisitante;
+int golesdeLocal;
+int golesdeVisitante;
+ResultadoEnum resultado;
+
+
+   
 }
--- Ejercicio1/PartidosAdmin.java
+++ Ejercicio1/PartidosAdmin.java
@@ -1,5 +1,20 @@
 package Ejercicio1;
 
+import java.util.List;
+
 public class PartidosAdmin {
+public List<Partido> filtrarPorEquipo(Equipo equipo);
+    /*: Devuelve todos los
+    partidos de un equipo.*/
+public Equipo fuerteDeLocal();
+    /* Devuelve el equipo que más ganó de local.*/
+public Equipo problemasDefensivos();
+   /* Devuelve el equipo que más goles
+    recibió.*/
+public Equipo deportivoEmpate();
+   /* Devuelve el equipo que más empató.*/
+public List<EquipoPuntaje> tablaDePosiciones();
+/* Devuelve en orden descendente los equipos junto con su puntaje.*/
+     
     
 }
