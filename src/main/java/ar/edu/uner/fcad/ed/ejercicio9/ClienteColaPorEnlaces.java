819ac81f539fb95e0605ebc285c84bc09c83b206
Stefano-Kiwi <stefanobudon@gmail.com>
2022-05-06 17:21:49 -0300

    Falta multiDequeue y exchange

--- src/main/java/ar/edu/uner/fcad/ed/ejercicio9/ClienteColaPorEnlaces.java
+++ src/main/java/ar/edu/uner/fcad/ed/ejercicio9/ClienteColaPorEnlaces.java
@@ -5,5 +5,20 @@
  * @author stefa
  */
 public class ClienteColaPorEnlaces {
-    
+    public static void main(String[] args) {
+        ColaPorEnlacesExt<String> cola = new ColaPorEnlacesExt<String>();
+        cola.enqueue("arbol");
+        cola.enqueue("perro");
+        cola.enqueue("gato");
+        cola.enqueue("auto");
+        cola.enqueue("pez");
+        cola.enqueue("auto");
+        cola.enqueue("gato");
+        cola.enqueue("auto");
+        
+        System.out.println("Tamanio: "+cola.size());
+        System.out.println("toString: "+cola.toString());
+        cola.reemplazarTodos("auto", "batman");
+        System.out.println("Reemplazo auto por batman: "+cola.toString());
+    }
 }
